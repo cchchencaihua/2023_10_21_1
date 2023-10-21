09a00a971606456d2f1dc6668d5caa2481a7a817
caihua <2120271753@qq.com>
2023-10-21 18:30:01 +0800

    测试代码

---
+++ test 6-15/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("hehe ±»Ãÿ\n");
+//	return 0;
+//
+//int main()
+//{
+//	char ch = 'A';
+//	int age = 20;
+//	long lo = 100;
+//	float f = 5.0;
+//	double d = 3.14;
+//	printf("%lf", d);
+//	printf("%f\n", f);
+//	printf("%d\n", lo);
+//	printf("%d\n", age);
+//	printf("%c", ch);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+
+int main()
+{
+	
+	return 0;
+}
